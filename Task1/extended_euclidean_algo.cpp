c31c117945faa4365082de3ad42435e7090c7825
afifamaisha13 <afifamaisha13@gmail.com>
2026-05-05 23:25:00 +0600

    Implement Extended Euclidean Algorithm in C++

---
+++ Task1/extended_euclidean_algo.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// Function to compute gcd and coefficients x, y
+int extendedGCD(int a, int b, int &x, int &y) {
+    // Base case
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+
+    int x1, y1;
+    int gcd = extendedGCD(b, a % b, x1, y1);
+
+    // Update x and y
+    x = y1;
+    y = x1 - (a / b) * y1;
+
+    return gcd;
+}
+
+int main() {
+    int a, b, x, y;
+
+    cout << "Enter two numbers (a and b): ";
+    cin >> a >> b;
+
+    int gcd = extendedGCD(a, b, x, y);
+
+    cout << "GCD = " << gcd << endl;
+    cout << "x = " << x << endl;
+    cout << "y = " << y << endl;
+
+    // Verify result
+    cout << "Verification: " << a << "(" << x << ") + "
+         << b << "(" << y << ") = " << a*x + b*y << endl;
+
+    return 0;
+}
